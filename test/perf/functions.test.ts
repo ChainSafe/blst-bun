2ea4b5c719614abc64878aacf0211f3ba608c28c
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-03-07 15:02:15 +0700

    chore: add comments for aggregateWithRandomness()

--- test/perf/functions.test.ts
+++ test/perf/functions.test.ts
@@ -28,7 +28,10 @@ describe("functions", () => {
 		}
 	});
 	describe("aggregateWithRandomness", () => {
-		for (const count of [1, 16, 128, 256, 512, 1024]) {
+		// for (const count of [1, 16, 128, 256, 512, 1024]) {
+    // TODO: blst-bun supports up to 128 sets while blst-ts supports unlimited sets
+    // as of Feb 2025, this is enough for lodestar
+		for (const count of [1, 16, 128]) {
 			bench({
 				id: `aggregateWithRandomness - ${count} sets`,
 				before: () => {
