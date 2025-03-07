832e459a15700e4722da0207bc9890266b0b6903
Tuyen Nguyen <twoeths@users.noreply.github.com>
2025-03-07 14:53:50 +0700

    feat: aggregate unlimited pubkeys/signatures

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -15,19 +15,25 @@ const signaturesRef = new Uint32Array(MAX_AGGREGATE_PER_JOB * 2);
  * If `pks_validate` is `true`, the public keys will be infinity and group checked.
  */
 export function aggregatePublicKeys(pks: Array<PublicKey>, pksValidate?: boolean | undefined | null): PublicKey {
-	if (pks.length > MAX_AGGREGATE_PER_JOB) {
-		throw new Error("Too many public keys");
-	}
+  if (pks.length === 0) {
+    throw new Error("At least one public key is required");
+  }
 
-	const pksRef = writePublicKeysReference(pks);
+	const resultPks: PublicKey[] = []
 
-	const defaultPk = PublicKey.defaultPublicKey();
-	const res = binding.aggregatePublicKeys(defaultPk.blst_point, pksRef, pks.length, pksValidate ?? false);
-	if (res !== 0) {
-		throw new Error(`Failed to aggregate public keys: ${res}`);
-	}
+  for (let i = 0; i < pks.length; i += MAX_AGGREGATE_PER_JOB) {
+    const pksBatch = pks.slice(i, Math.min(pks.length, i + MAX_AGGREGATE_PER_JOB));
+    const pksRef = writePublicKeysReference(pksBatch);
+    const outPk = PublicKey.defaultPublicKey();
+    const res = binding.aggregatePublicKeys(outPk.blst_point, pksRef, pksBatch.length, pksValidate ?? false);
 
-	return defaultPk;
+    if (res !== 0) {
+      throw new Error(`Failed to aggregate public keys: ${res}`);
+    }
+    resultPks.push(outPk);
+  }
+
+  return resultPks.length === 1 ? resultPks[0] : aggregatePublicKeys(resultPks, pksValidate);
 }
 
 /**
@@ -36,19 +42,25 @@ export function aggregatePublicKeys(pks: Array<PublicKey>, pksValidate?: boolean
  * If `sigs_groupcheck` is `true`, the signatures will be group checked.
  */
 export function aggregateSignatures(sigs: Array<Signature>, sigsGroupcheck?: boolean | undefined | null): Signature {
-	if (sigs.length > MAX_AGGREGATE_PER_JOB) {
-		throw new Error("Too many signatures");
-	}
+  const resultSig: Signature[] = []
 
-	const sigsRef = writeSignaturesReference(sigs);
+  if (sigs.length === 0) {
+    throw new Error("At least one signature is required");
+  }
 
-	const defaultSig = Signature.defaultSignature();
-	const res = binding.aggregateSignatures(defaultSig.blst_point, sigsRef, sigs.length, sigsGroupcheck ?? false);
-	if (res !== 0) {
-		throw new Error(`Failed to aggregate signatures: ${res}`);
-	}
+  for (let i = 0; i < sigs.length; i += MAX_AGGREGATE_PER_JOB) {
+    const sigsBatch = sigs.slice(i, Math.min(sigs.length, i + MAX_AGGREGATE_PER_JOB));
+    const sigsRef = writeSignaturesReference(sigsBatch);
+    const outSig = Signature.defaultSignature();
+    const res = binding.aggregateSignatures(outSig.blst_point, sigsRef, sigsBatch.length, sigsGroupcheck ?? false);
 
-	return defaultSig;
+    if (res !== 0) {
+      throw new Error(`Failed to aggregate signatures: ${res}`);
+    }
+    resultSig.push(outSig);
+  }
+
+  return resultSig.length === 1 ? resultSig[0] : aggregateSignatures(resultSig, sigsGroupcheck);
 }
 
 /**
@@ -60,29 +72,25 @@ export function aggregateSerializedPublicKeys(
 	pks: Array<Uint8Array>,
 	pksValidate?: boolean | undefined | null
 ): PublicKey {
-	if (pks.length > MAX_AGGREGATE_PER_JOB) {
-		throw new Error("Too many public keys");
-	}
-
 	if (pks.length < 1) {
 		throw new Error("At least one public key is required");
 	}
 
-	const pksRef = writeSerializedPublicKeysReference(pks);
+  const resultPublicKeys: PublicKey[] = []
 
-	const defaultPk = PublicKey.defaultPublicKey();
-	const res = binding.aggregateSerializedPublicKeys(
-		defaultPk.blst_point,
-		pksRef,
-		pks.length,
-		pks[0].length,
-		pksValidate ?? false
-	);
-	if (res !== 0) {
-		throw new Error(`Failed to aggregate serialized public keys: ${res}`);
-	}
+  for (let i = 0; i < pks.length; i += MAX_AGGREGATE_PER_JOB) {
+    const pksBatch = pks.slice(i, Math.min(pks.length, i + MAX_AGGREGATE_PER_JOB));
+    const pksRef = writeSerializedPublicKeysReference(pksBatch);
+    const outPk = PublicKey.defaultPublicKey();
+    const res = binding.aggregateSerializedPublicKeys(outPk.blst_point, pksRef, pksBatch.length, pks[0].length, pksValidate ?? false);
 
-	return defaultPk;
+    if (res !== 0) {
+      throw new Error(`Failed to aggregate serialized public keys: ${res}`);
+    }
+    resultPublicKeys.push(outPk);
+  }
+
+  return resultPublicKeys.length === 1 ? resultPublicKeys[0] : aggregatePublicKeys(resultPublicKeys, pksValidate);
 }
 
 /**
@@ -94,29 +102,25 @@ export function aggregateSerializedSignatures(
 	sigs: Array<Uint8Array>,
 	sigsGroupcheck?: boolean | undefined | null
 ): Signature {
-	if (sigs.length > MAX_AGGREGATE_PER_JOB) {
-		throw new Error("Too many signatures");
-	}
-
 	if (sigs.length < 1) {
 		throw new Error("At least one signature is required");
 	}
 
-	const sigsRef = writeSerializedSignaturesReference(sigs);
+  const resultSignatures: Signature[] = [];
 
-	const defaultSig = Signature.defaultSignature();
-	const res = binding.aggregateSerializedSignatures(
-		defaultSig.blst_point,
-		sigsRef,
-		sigs.length,
-		sigs[0].length,
-		sigsGroupcheck ?? false
-	);
-	if (res !== 0) {
-		throw new Error(`Failed to aggregate serialized signatures: ${res}`);
-	}
+  for (let i = 0; i < sigs.length; i += MAX_AGGREGATE_PER_JOB) {
+    const sigsBatch = sigs.slice(i, Math.min(sigs.length, i + MAX_AGGREGATE_PER_JOB));
+    const sigsRef = writeSerializedSignaturesReference(sigsBatch);
+    const outSig = Signature.defaultSignature();
+    const res = binding.aggregateSerializedSignatures(outSig.blst_point, sigsRef, sigsBatch.length, sigs[0].length, sigsGroupcheck ?? false);
 
-	return defaultSig;
+    if (res !== 0) {
+      throw new Error(`Failed to aggregate serialized signatures: ${res}`);
+    }
+    resultSignatures.push(outSig);
+  }
+
+  return resultSignatures.length === 1 ? resultSignatures[0] : aggregateSignatures(resultSignatures, sigsGroupcheck);
 }
 
 function writeSerializedPublicKeysReference(pks: Uint8Array[]): Uint32Array {
